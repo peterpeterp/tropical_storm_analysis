779500ab226f6925ea2bddcc37f2740bc9e19fc2
Peter <peterpfleiderer@gmx.de>
2018-02-23 12:51:52 +0000

    distinction between tracks

--- TC_detection_CAM25.py
+++ TC_detection_CAM25.py
@@ -281,7 +281,7 @@ class tc_tracks(object):
         summary={'not':[],'Cat1':[],'Cat2':[],'Cat3':[],'Cat4':[],'Cat5':[]}
         for id_,track in self._tcs.items():
             track=np.array(track[np.isfinite(track[:,'t']),:],dtype=np.int)
-            #tmp.append(self.plot_on_map(self._m,track[:,2],track[:,1],linestyle='',marker='.'))
+            tmp.append(self.plot_on_map(self._m,track[:,2],track[:,1],linestyle='',marker='o'))
             tmp+=self.plot_on_map(self._m,track[:,2],track[:,1],z=self._MSLP.values[track[:,0],track[:,1],track[:,2]])
             print(self._MSLP.values[track[:,0],track[:,1],track[:,2]].min())
             summary[self.tc_cat(self._MSLP.values[track[:,0],track[:,1],track[:,2]].min())].append(id_)
