ea5c27f317a0e299aa1e37da760b619a1f4c8d59
Peter <peterpfleiderer@gmx.de>
2018-02-26 11:58:46 +0000

    checking

--- TC_summary_CAM25.py
+++ TC_summary_CAM25.py
@@ -42,7 +42,10 @@ if os.path.isfile('detection/CAM25_all_tracks.nc')==False:
         if len(tmp.values())>0:
             # check for duplicates
             track=tmp[tmp.keys()[0]]
+            print tmp
+            print track
             track=np.array(track[np.isfinite(track[:,'t']),:])
+            print track
             x_=[int(xx) for xx in track[:,'x']]
             if x_ in xxx:
                 used=identifieres.ID[xxx.index(x_)]
