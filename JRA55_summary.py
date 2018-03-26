05566269382f1be27c3719522565f5d62e5f5b74
Peter <peterpfleiderer@gmx.de>
2018-03-26 12:42:35 +0100

    new summary

--- JRA55_summary.py
+++ JRA55_summary.py
@@ -42,23 +42,15 @@ if os.path.isfile('detection/JRA55/JRA55_obs_track_info.nc')==False or overwrite
     obs_tracks=da.Dataset({'obs_tracks':obs_tracks})
     obs_tracks.write_nc('detection/JRA55/JRA55_obs_track_info.nc',mode='w')
 
-obs_tracks=da.read_nc('detection/JRA55/JRA55_obs_track_info.nc')['obs_tracks']
+for style in ['contours','knutson2007']:
+    if os.path.isfile('detection/JRA55/JRA55_all_tracks_'+style+'.nc')==False or overwrite:
+        # check for duplicates
+        all_tracks={}
+        for identifier in [str(yr) for yr in range(1979,2017)]:
+            print(all_tracks)
+            tmp=da.read_nc('detection/JRA55/'+str(identifier)+'_JRA55/track_info_'+style+'.nc')['track_info']
+            for storm in tmp.storm:
+                all_tracks[storm]=tmp[storm,:,:]
 
-categories={}
-for cat in range(6):
-    pos=np.where(obs_tracks==cat)
-    categories[cat]=obs_tracks.ix[pos[0],pos[1],:]
-
-
-tc_colors={0:'lightblue',1:'#ffffcc',2:'#ffe775',3:'#ffc148',4:'#ff8f20',5:'#ff6060'}
-fig,axes=plt.subplots(nrows=2,ncols=2)
-for ax,vari in zip(axes.flatten(),['MSLP','VO','Wind10','T']):
-    for cat in range(5):
-        tmp=categories[cat][:,:,vari].values
-        tmp=tmp[np.isfinite(tmp)]
-        hist=np.histogram(tmp,bins=20,density=True)
-        ax.plot(hist[1][:-1],hist[0],color=tc_colors[cat])
-    ax.set_xlabel(vari)
-
-plt.tight_layout()
-plt.savefig('detection/JRA55/JRA55_obs_hists.png')
+        all_tracks=da.Dataset({'all_tracks':all_tracks})
+        all_tracks.write_nc('detection/JRA55/JRA55_all_tracks_'+style+'.nc',mode='w')
---
+++ plot_JRA55.py
@@ -0,0 +1,97 @@
+from __future__ import print_function
+import os,sys,glob,time,collections,gc,calendar,weakref,resource
+from netCDF4 import Dataset,netcdftime,num2date
+import dimarray as da
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy.ndimage as ndimage
+from shapely.geometry.polygon import Polygon
+import matplotlib.ticker as mticker
+import cartopy
+
+import cartopy.crs as ccrs
+
+os.chdir('/Users/peterpfleiderer/Documents/Projects/tropical_cyclones/')
+sys.path.append('/Users/peterpfleiderer/Documents/Projects/tropical_cyclones/tc_detection')
+import TC_support ;  TC_support = reload(TC_support)
+
+
+nc=da.read_nc('data/CAR25/item16222_6hrly_inst/item16222_6hrly_inst_p014_2017-06_2017-10.nc')
+grid_lats = nc['latitude1'].values
+grid_lons = nc['longitude1'].values
+o_lon_p = nc['rotated_pole1'].attrs['grid_north_pole_longitude']
+o_lat_p = nc['rotated_pole1'].attrs['grid_north_pole_latitude']
+lon_0 = TC_support.normalize180(o_lon_p-180.)
+
+rot_pole = ccrs.RotatedPole(pole_longitude=o_lon_p, pole_latitude=o_lat_p)
+plate_carree = ccrs.PlateCarree()
+globe= ccrs.Orthographic(central_longitude=-60.0, central_latitude=20.0, globe=None)
+
+cat_colors={0:'lightblue',1:'#ffffcc',2:'#ffe775',3:'#ffc148',4:'#ff8f20',5:'#ff6060'}
+
+obs_tracks=da.read_nc('detection/JRA55/JRA55_all_tracks.nc')['all_tracks']
+nc=da.read_nc('data/JRA55/atl_2010_MSLP.nc')
+lons,lats=nc.lon,nc.lat
+
+# sparial stuff
+plt.close('all')
+plt.figure(figsize=(10,5))
+ax = plt.axes(projection=rot_pole)
+ax.set_global()
+ax.coastlines()
+gl=ax.gridlines(color='#8470ff',linewidth=1)
+gl.ylocator = mticker.FixedLocator(np.arange(-20,60,10))
+gl.xlocator = mticker.FixedLocator(np.arange(-120,20,10))
+for yy in np.arange(-10,40,10):   ax.text(-15,yy,str(yy),color='#8470ff',transform=plate_carree)
+for xx in np.arange(-100,0,10):   ax.text(xx,8,str(xx),color='#8470ff',transform=plate_carree)
+ax.add_feature(cartopy.feature.LAND, facecolor='darkgreen')
+ax.add_feature(cartopy.feature.OCEAN,facecolor='darkblue')
+ax.set_xlim(np.min(grid_lons)-5,np.max(grid_lons)+20)
+ax.set_ylim(np.min(grid_lats)-5,np.max(grid_lats)+5)
+reg=Polygon([(grid_lons[0],grid_lats[0]),(grid_lons[-1],grid_lats[0]),(grid_lons[-1],grid_lats[-1]),(grid_lons[0],grid_lats[-1]),(grid_lons[0],grid_lats[0])])
+ax.add_geometries([reg], rot_pole, color='lightgreen',alpha=1,facecolor='none')
+
+for storm in tc_sel.storm:
+    ax.plot(tc_lon[storm,:],tc_lat[storm,:],color=cat_colors[tc_cat(tc_wind[storm,:,0].max(),'wind')],alpha=0.2,linewidth=1,transform=plate_carree)
+
+plt.title('ibtracks')
+plt.tight_layout()
+plt.savefig('plots/ibtracks/ibtracks_tracks.png',dpi=300)
+
+ax.lines=[]
+for storm in tc_sel.storm:
+    ax.plot(tc_lon[storm,0],tc_lat[storm,0],color=cat_colors[tc_cat(tc_wind[storm,:,0].max(),'wind')],alpha=0.3,marker='o',transform=plate_carree)
+
+plt.title('ibtracks')
+plt.tight_layout()
+plt.savefig('plots/ibtracks/ibtracks_genesis.png',dpi=300)
+
+
+# statistics
+hurrs_in_seas=[]
+for year in range(1979,2018):
+    tc_clim=tc_sel.ix[tc_sel['season']==year]
+    clim_cat=np.array(TC_support.tc_cat(np.nanmax(tc_clim['source_wind'],axis=(1,2)),'wind'))
+    hurrs_in_seas.append(np.sum(clim_cat>0))
+
+tc_clim=tc_sel.ix[tc_sel['season']>=1979]
+plt.close('all')
+plt.figure(figsize=(5,3))
+clim_cat=np.array(TC_support.tc_cat(np.nanmax(tc_clim['source_wind'],axis=(1,2)),'wind'))
+for cat in range(1,6):
+    plt.bar(cat,np.sum(clim_cat==cat)/float(2017-1979),color=cat_colors[cat])
+plt.text(4.2,2.5,'total: '+str(int(round(np.mean(hurrs_in_seas))))+' $\pm$ '+str(int(round(np.std(hurrs_in_seas)))))
+plt.ylabel('hurricanes per season')
+plt.xlabel('hurricane category')
+plt.title('ibtracks')
+plt.tight_layout()
+plt.savefig('plots/ibtracks/ibtracks_cat_hist.png',dpi=300)
+
+
+
+
+
+#
