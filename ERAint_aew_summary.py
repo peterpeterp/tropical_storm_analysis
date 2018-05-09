77fbd6931e7c0f5a7851321867d27775a5024152
Peter <peterpfleiderer@gmx.de>
2018-05-09 12:28:46 +0200

    direct summary plot

--- ERAint_aew_summary.py
+++ ERAint_aew_summary.py
@@ -21,25 +21,31 @@ except:
     os.chdir('/p/projects/tumble/carls/shared_folder/TC_detection/')
     local=False
 
-sys.path.append('/Users/peterpfleiderer/Documents/Projects/tropical_cyclones/tc_detection')
-sys.path.append('/p/projects/tumble/carls/shared_folder/TC_detection/tc_detection')
-from TC_support import *
+nc=da.read_nc('easterly_waves/ano_curv_vort_850-700-600_2008.nc')
+lons,lats=nc.lon,nc.lat
+lons[lons>180]-=360
 
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("--overwrite",'-o', help="overwrite output files",action="store_true")
-args = parser.parse_args()
+plt.close('all')
+plate_carree = ccrs.PlateCarree()
+fig,axes=plt.subplots(nrows=2,ncols=1,figsize=(10,10),subplot_kw={'projection': plate_carree})
+for ax in axes:
+    ax.set_global()
+    ax.coastlines()
+    ax.set_xlim(np.min(lons),np.max(lons))
+    ax.set_ylim(np.min(lats),np.max(lats))
 
 
 for style in ['belanger']:
     if os.path.isfile('aew_detection/ERAint/ERAint_all_tracks_AEW_'+style+'.nc')==False or overwrite:
         # check for duplicates
-        all_tracks={}
         for identifier in [str(yr) for yr in range(1979,2017)]:
             tmp=da.read_nc('aew_detection/ERAint/'+str(identifier)+'/track_info_'+style+'.nc')
             for id_,track in tmp.items():
                 if id_ not in ['z','time']:
-                    all_tracks[id_]=track
+                    track=track[np.isfinite(track[:,'t']),:]
+                    axes[0].plot(lons[np.array(track[:,'x'],int)],lats[np.array(track[:,'y'],int)],color='orange',alpha=0.5,linewidth=2,transform=plate_carree)
+                    axes[1].plot(lons[np.array(track[0,'x'],int)],lats[np.array(track[0,'y'],int)],color='orange',alpha=0.5,linestyle='',transform=plate_carree)
 
-        all_tracks=da.Dataset({'all_tracks':all_tracks})
-        all_tracks.write_nc('aew_detection/ERAint/ERAint_all_tracks_AEW_'+style+'.nc',mode='w')
+
+plt.tight_layout()
+plt.savefig('plots/ERAint/ERAint_AEW_tracks.png',dpi=300)
