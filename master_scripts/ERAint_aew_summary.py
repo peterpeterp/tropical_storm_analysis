6a0f0c0ef8b93725a3300b8c9379ce67426ad149
Peter <peterpfleiderer@gmx.de>
2018-05-14 12:20:18 +0200

    seperate styles

--- master_scripts/ERAint_aew_summary.py
+++ master_scripts/ERAint_aew_summary.py
@@ -35,18 +35,19 @@ lat=lat[:,0]; lat=lat[np.isfinite(lat)]
 lon=lon[0,:]; lon=lon[np.isfinite(lon)]
 lons,lats=np.meshgrid(lon,lat)
 
-plt.close('all')
-plate_carree = ccrs.PlateCarree()
-fig,axes=plt.subplots(nrows=4,ncols=1,figsize=(8,6),subplot_kw={'projection': plate_carree},gridspec_kw = {'height_ratios':[3,1,3,1]})
-for ax in axes[[0,2]]:
-    ax.set_global()
-    ax.coastlines()
-    ax.set_xlim(np.min(lon),np.max(lon))
-    ax.set_ylim(np.min(lat),np.max(lat))
 
-occurence=lats.copy()*0.0
-genesis=lats.copy()*0.0
 for style in ['dieng','belanger']:
+    plt.close('all')
+    plate_carree = ccrs.PlateCarree()
+    fig,axes=plt.subplots(nrows=4,ncols=1,figsize=(8,6),subplot_kw={'projection': plate_carree},gridspec_kw = {'height_ratios':[3,1,3,1]})
+    for ax in axes[[0,2]]:
+        ax.set_global()
+        ax.coastlines()
+        ax.set_xlim(np.min(lon),np.max(lon))
+        ax.set_ylim(np.min(lat),np.max(lat))
+
+    occurence=lats.copy()*0.0
+    genesis=lats.copy()*0.0
     if os.path.isfile('aew_detection/ERAint/ERAint_all_tracks_AEW_'+style+'.nc')==False or overwrite:
         # check for duplicates
         for identifier in [str(yr) for yr in years]:
@@ -58,20 +59,20 @@ for style in ['dieng','belanger']:
                     for point in track.values.tolist():
                         occurence[int(point[1]),int(point[2])]+=1
 
-occurence/=float(len(years))
-cs=axes[0].contourf(lons,lats,occurence)
-axes[1].outline_patch.set_edgecolor('white')
-cbar_ax=fig.add_axes([0,0.6,1,0.15])
-cbar_ax.axis('off')
-cb=fig.colorbar(cs,orientation='horizontal',label='seasonal occurence frequency',ax=cbar_ax)
+    occurence/=float(len(years))
+    cs=axes[0].contourf(lons,lats,occurence)
+    axes[1].outline_patch.set_edgecolor('white')
+    cbar_ax=fig.add_axes([0,0.6,1,0.15])
+    cbar_ax.axis('off')
+    cb=fig.colorbar(cs,orientation='horizontal',label='seasonal occurence frequency',ax=cbar_ax)
 
-genesis/=float(len(years))
-cs=axes[2].contourf(lons,lats,genesis)
-axes[3].outline_patch.set_edgecolor('white')
-cbar_ax=fig.add_axes([0,0.1,1,0.15])
-cbar_ax.axis('off')
-cb=fig.colorbar(cs,orientation='horizontal',label='seasonal genesis frequency',ax=cbar_ax)
+    genesis/=float(len(years))
+    cs=axes[2].contourf(lons,lats,genesis)
+    axes[3].outline_patch.set_edgecolor('white')
+    cbar_ax=fig.add_axes([0,0.1,1,0.15])
+    cbar_ax.axis('off')
+    cb=fig.colorbar(cs,orientation='horizontal',label='seasonal genesis frequency',ax=cbar_ax)
 
 
-plt.tight_layout()
-plt.savefig('plots/ERAint/ERAint_AEW_tracks.png',dpi=300)
+    plt.tight_layout()
+    plt.savefig('plots/ERAint/ERAint_AEW_tracks'+style+'.png',dpi=300)
