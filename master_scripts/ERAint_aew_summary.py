9eb18442ac3e4705da65f8a39082057ea5fd5001
peterpeterp <peterpfleiderer@gmx.de>
2019-01-02 12:26:03 +0100

    extract track info

--- master_scripts/ERAint_aew_summary.py
+++ master_scripts/ERAint_aew_summary.py
@@ -24,12 +24,22 @@ except:
     os.chdir('/p/projects/tumble/carls/shared_folder/tropical_cyclones/')
     local=False
     years=range(1979,2017)
+    data_path='/p/projects/tumble/carls/shared_folder/data/reanalysis/ERAint/'
     plt.switch_backend('agg')
 
+yrFr = np.array([xx/365./4. +152./365.  for xx in np.arange(0,732,1)])
+day = np.array([xx/4 for xx in np.arange(0,732,1)])
+month = day.copy() * 0
+month[day<30] = 6
+month[(day>=30) & (day<61)] = 7
+month[(day>=61) & (day<92)] = 8
+month[(day>=92) & (day<122)] = 9
+month[(day>=122) & (day<153)] = 10
+month[day>=153] = 11
 
 cmap = mpl.colors.LinearSegmentedColormap.from_list("", ["blue","green","yellow","red","brown","violet","white"])
 
-for style in ['dieng','belanger']:
+for style in ['belanger']: #'dieng'
     print(style)
     nc = da.read_nc('aew_detection/ERAint/2008/2008_'+style+'_grid.nc')
     lat=nc['lats'].values
@@ -49,16 +59,42 @@ for style in ['dieng','belanger']:
 
     occurence=lats.copy()*0.0
     genesis=lats.copy()*0.0
-    if os.path.isfile('aew_detection/ERAint/ERAint_all_tracks_AEW_'+style+'.nc')==False or overwrite:
+
+    _time = np.zeros([100,1]) * np.nan
+    _month = np.zeros([100,1]) * np.nan
+    _lon = np.zeros([100,1]) * np.nan
+    _lat = np.zeros([100,1]) * np.nan
+    _ids = []
+
+    if os.path.isfile('aew_detection/ERAint/ERAint_all_tracks_AEW_'+style+'.nc')==False or True:
         # check for duplicates
         for identifier in [str(yr) for yr in years]:
             tmp=da.read_nc('aew_detection/ERAint/'+str(identifier)+'/'+str(identifier)+'_'+style+'_track_info.nc')
             for id_,track in tmp.items():
                 if id_ not in ['z','time']:
+                    _ids.append(id_)
                     track=track[np.isfinite(track[:,'t']),:]
                     genesis[int(track.ix[0,1]),int(track.ix[0,2])]+=1
                     for point in track.values.tolist():
                         occurence[int(point[1]),int(point[2])]+=1
+
+                    _lon = np.vstack((_lon,_lon[:,0].copy()))
+                    _lon[:track.shape[0],-1] = lons[np.array(track.ix[:,1],np.int),np.array(track.ix[:,2],np.int)]
+                    _lat = np.vstack((_lat,_lat[:,0].copy()))
+                    _lat[:track.shape[0],-1] = lats[np.array(track.ix[:,1],np.int),np.array(track.ix[:,2],np.int)]
+
+                    _time = np.vstack((_time,_time[:,0].copy()))
+                    _time[:track.shape[0],-1] = yrFr[track[:,'t']]+float(identifier)
+                    _month = np.vstack((_month,_month[:,0].copy()))
+                    _month[:track.shape[0],-1] = month[track[:,'t']]
+
+        ds = da.Dataset({
+            'lon':da.DimArray(_lon,axes=[np.arange(0,100,1,np.short),np.array(_ids)],dims=['step','ID']),
+            'lat':da.DimArray(_lat,axes=[np.arange(0,100,1,np.short),np.array(_ids)],dims=['step','ID']),
+            'time':da.DimArray(_time,axes=[np.arange(0,100,1,np.short),np.array(_ids)],dims=['step','ID']),
+            'month':da.DimArray(_month,axes=[np.arange(0,100,1,np.short),np.array(_ids)],dims=['step','ID']),
+        })
+        ds.write_nc('aew_detection/ERAint/ERAint_all_tracks_AEW_'+style+'_clean.nc')
 
     occurence/=float(len(years))
     cs=axes[0].contourf(lons,lats,occurence,20,cmap=cmap)
